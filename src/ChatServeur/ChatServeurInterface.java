1805f04135b6c91cb13e73f43a0f747ded17bae9
Bastien LEMALE <bastien.lemale@eleves.ec-nantes.fr>
2011-12-09 10:39:58 +0100

    Modif ChatServeurInterface

--- src/ChatServeur/ChatServeurInterface.java
+++ src/ChatServeur/ChatServeurInterface.java
@@ -2,11 +2,12 @@ package ChatServeur;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.SortedSet;
 
 public interface ChatServeurInterface extends Remote{
 	
 	boolean login(String id) throws RemoteException;
 	boolean logout(String id) throws RemoteException;
-	SortedSet<Message> updateMessage()
+	SortedSet<Message> updateMessage();
 
 }
